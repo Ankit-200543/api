321b2d53206869ea078fae5e1847691a06901176
Ankit-200543 <ak8946562@gmail.com>
2025-10-22 13:41:32 +0530

    second

---
+++ index.js
@@ -0,0 +1,12 @@
+const express=require('express')
+const app=express()
+const port=3000 || process.env.port
+
+
+app.get("/",(req,res)=>{
+    res.send("welcome to my webpage")
+})
+
+app.listen(port,()=>{
+    console.log("server Is started")
+})
