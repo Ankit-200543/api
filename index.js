df99044e384b00593f2834af16fd9e15752e720f
Ankit-200543 <ak8946562@gmail.com>
2025-10-27 20:08:50 +0530

    error fixed

--- index.js
+++ index.js
@@ -1,23 +1,36 @@
-const express=require('express')
-const app=express()
-const port=3000 || process.env.PORT;
-let RequestNo=0;
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
 
+const app = express();
+const server = http.createServer(app); // Create HTTP server
+const io = new Server(server); // Attach Socket.IO to server
 
-app.get("/",(req,res)=>{
-    RequestNo++
-    console.log("This Is Request No"+RequestNo);
-    res.send("Hello fello Users ! You are Number "+RequestNo)
-})
+const port = process.env.PORT || 3000;
+let RequestNo = 0;
+
+// Normal HTTP route
+app.get("/", (req, res) => {
+  RequestNo++;
+  console.log("This is Request No " + RequestNo);
+  res.send("Hello fellow Users! You are number " + RequestNo);
+});
+
+// Socket.IO part
 io.on("connection", (socket) => {
   console.log("User connected:", socket.id);
 
   socket.on("chat message", (msg) => {
     console.log("Message:", msg);
-    io.emit("chat message", msg);
+    io.emit("chat message", msg); // Broadcast to everyone
+  });
+
+  socket.on("disconnect", () => {
+    console.log("User disconnected:", socket.id);
   });
 });
 
-app.listen(port,()=>{
-    console.log("server Is started")
-})
+// Start the server
+server.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
