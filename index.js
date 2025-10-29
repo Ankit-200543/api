21b02e56704f8bd600c94480487925a55b340180
Ankit-200543 <ak8946562@gmail.com>
2025-10-29 15:07:42 +0530

    avaialble user

--- index.js
+++ index.js
@@ -1,51 +1,40 @@
-const express = require('express');
-const http = require('http');
-const { Server } = require('socket.io');
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
 
 const app = express();
-const server = http.createServer(app); // Create HTTP server
-const io = new Server(server); // Attach Socket.IO to server
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "*", // allow all origins for testing
+  },
+});
+const userd="ankit"
 
-const port = process.env.PORT || 3000;
 let RequestNo = 0;
 
-// Normal HTTP route
 app.get("/", (req, res) => {
   RequestNo++;
   console.log("This is Request No " + RequestNo);
-  res.send("Hello fellow Users! You are number " + RequestNo);
+  res.send("Hello fellow users! You are number " + RequestNo);
 });
 
-
-
-
-app.get("/login/:username/:password", (req, res) => {
-  const { username, password } = req.params;
-  console.log("one login request has arrived")
-
-  // Example logic (you can replace this with DB validation)
-  if (username === "ankit" && password === "1234") {
-    res.send("Login successful! Welcome " + username);
-  } else {
-    res.send("Invalid username or password");
-  }
-});
-
-// Socket.IO part
 io.on("connection", (socket) => {
   console.log("User connected:", socket.id);
 
-  socket.on("chat message", (msg) => {
-    console.log("Message:", msg);
-    io.emit("chat message", msg); // Broadcast to everyone
-  });
+  socket.on("checkAvialableUsername", (username) => {
+    if(userd!=username){
+          socket.emit("Yes")
+    }
+    else{
+                socket.emit("No")
 
-  socket.on("disconnect", () => {
-    console.log("User disconnected:", socket.id);
+    }
+   
   });
 });
 
-// Start the server
-server.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log("Server started on port", PORT);
 });
