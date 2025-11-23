f48c7c0968f7805d2eed16e9fa2af095d559d341
Ankit-200543 <ak8946562@gmail.com>
2025-11-23 13:55:15 +0530

    fifth

--- index.js
+++ index.js
@@ -12,12 +12,17 @@ app.get("/getWeather/:latitude/:longitude", async (req, res) => {
             `https://api.open-meteo.com/v1/forecast?latitude=${req.params.latitude}&longitude=${req.params.longitude}&current=temperature_2m,wind_speed_10m,relative_humidity_2m`
         );
         const weatherData = await weatherInfo.json();
+        const location=await fetch(`https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${req.params.latitude}&longitude=${req.params.longitude}&localityLanguage=en`);
+        const locationInfo = await location.json();
+        const address=`${locationInfo.city},${locationInfo.principalSubdivision},${locationInfo.countryName}`;
+
 
 
 
     
 
         const filtered = {
+            pata:address,
             Temperature: weatherData.current.temperature_2m,
             windSpeed: weatherData.current.wind_speed_10m,
             Humidity: weatherData.current.relative_humidity_2m
